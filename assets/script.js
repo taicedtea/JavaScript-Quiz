15c50d7d4c2221de469c619c1944119d77516ab2
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-02-26 22:07:37 -0500

    Reordered landed page, added ids and variable selectors, slight css updates

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
 // Assign variables
-
+let timer = document.querySelector('#timer');
+let highScoreBtn = document.querySelector('#startBtn');
+let startBtn = document.querySelector('#startBtn');
+let questionBox = document.querySelector('#startBtn');
+let question = document.querySelector('#questionBox');
+let answerList = document.querySelector('#answerList');
+let endBox = document.querySelector('#endBox');
+let playerScore = document.querySelector('#playerScore');
