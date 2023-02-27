821be4fab63a3b5b38dc97523ddf34e3fcd5996e
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-02-26 22:28:32 -0500

    Added start button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
 // Assign variables
 let timer = document.querySelector('#timer');
 let highScoreBtn = document.querySelector('#startBtn');
+let infoBox = document.querySelector('#infoBox');
 let startBtn = document.querySelector('#startBtn');
-let questionBox = document.querySelector('#startBtn');
-let question = document.querySelector('#questionBox');
+let questionBox = document.querySelector('#questionBox');
+let question = document.querySelector('#question');
 let answerList = document.querySelector('#answerList');
 let endBox = document.querySelector('#endBox');
 let playerScore = document.querySelector('#playerScore');
+
+//gives startBtn functionality
+startBtn.addEventListener('click', startGame);
+function startGame() {
+    infoBox.style.display = 'none';
+    questionBox.style.display = 'block';
+    playerScore = 0; //makes sure score starts at 0
+    newQuestions();
+}
+
+function newQuestions() {
+
+}
+
