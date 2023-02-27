e40c69b4237c75917ab5771e787647c3cf0046d5
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-02-26 21:11:30 -0500

    Added  questions.js

---
+++ assets/questions.js
@@ -0,0 +1,28 @@
+// Quiz Questions
+let quizQuestions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices:["js", "javascript", "scripts", "script"],
+        answer:"script",
+    },
+    {
+        question: "Commonly used data types are NOT which of the following?",
+        choices:["boolean", "data", "number", "string"],
+        answer:"data",
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        choices:["var", "let", "Both A and B", "None of the above"],
+        answer:"Both A and B",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ______.",
+        choices:["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+        answer:"Parenthesis",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        choices:["Number and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer:"All of the Above",
+    },
+]
