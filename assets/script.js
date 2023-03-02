4ff6faaa966702e37a568fb890bd6cbd58358964
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-03-01 21:10:52 -0500

    Added scoring functionality, questions now change, end page with player score added

--- assets/script.js
+++ assets/script.js
@@ -14,21 +14,20 @@ let correct = document.querySelector('#correct');
 let incorrect = document.querySelector('#incorrect');
 let playerScore = document.querySelector('#playerScore');
 
+let score = 0;
 //gives startBtn functionality
 startBtn.addEventListener('click', startGame);
 function startGame() {
     infoBox.style.display = 'none';
     questionBox.style.display = 'block';
-    playerScore = 0; //makes sure score starts at 0
     newQuestions();
     countDown();
-    checkAnwer();
-    playerScore++; // clicking starts gives negative score, this sets score back to 0
+    checkAnswer();
 }
 
 //thank you Mikhail on stackoverflow, this is for the countdown timer
+var sec = 45;
 function countDown(){
-    var sec = 45;
     setInterval(function(){
         timer.innerHTML='00:'+sec;
         sec--;
@@ -42,33 +41,47 @@ var i = 0 //to set question to first in index
 
 // populates question box
 function newQuestions() {
-    question.innerHTML = quizQuestions[i].question;
-    answerList.innerHTML = 
+    if (i < quizQuestions.length) {
+        question.innerHTML = quizQuestions[i].question;
+        answerList.innerHTML = 
         `<li>${quizQuestions[i].choices[0]}</li>
         <li>${quizQuestions[i].choices[1]}</li>
         <li>${quizQuestions[i].choices[2]}</li>
         <li>${quizQuestions[i].choices[3]}</li>`
+    } else {
+        console.log('done');
+        questionBox.style.display = "none";
+        endBox.style.display = "block";
+        playerScore.innerHTML = `Your score is: ${score}!`;
+    }
 }
 
 //function to check if chosen answer is correct or incorrect
-function checkAnwer() {
+function checkAnswer() {
     //gives element user clicked, thanks alex on stackoverflow
-    document.addEventListener('click', function(e) {
-        e = e || window.event;
-        var target = e.target || e.srcElement,
-        clickedAnswer = target.textContent || target.innerText;
+    questionBox.addEventListener('click', function(e) {
+        e = window.event;
+        var target = e.target,
+        clickedAnswer = target.textContent;
         console.log(clickedAnswer)
         //compares clicked element to correct answer
-        if (clickedAnswer == quizQuestions[i].answer) {
+        if (clickedAnswer === quizQuestions[i].answer) {
             console.log("correct");
-            playerScore++;
+            score++;
+            i++;
+            correct.style.display = "block";
+            incorrect.style.display = "none";
+            newQuestions();
+            //setTimeout(correct.style.display = "none", 5000 );
         } else {
             console.log("incorrect");
-            playerScore--;
+            sec -= 5;
+            i++;
+            correct.style.display = "none";
+            incorrect.style.display = "block";
+            newQuestions();
         }
-        console.log(playerScore);
-    }, false);
+        console.log("score: ",score);
+        console.log("i: ", i);
+    }, true);
 }
-
-
-
